e9603a3e4333b52063b4242c8a832f1dddcfaf65
Atulmahajan12345 <atulmahajan2299@gmail.com>
2022-04-01 15:20:40 +0530

    Welcome In Emp wage

---
+++ src/EmpWageComputationDay9/welcome.java
@@ -0,0 +1,11 @@
+package EmpWageComputationDay9;
+
+public class welcome {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome");
+		// TODO Auto-generated method stub
+
+	}
+
+}
